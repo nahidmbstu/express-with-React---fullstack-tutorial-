863fb8200b0488dfe64fe2f25852ef6156e98855
nahidhasan <nahidhasan@finder-lbs.com>
2018-08-08 18:25:21 +0600

    added proxy

--- config/prod.js
+++ config/prod.js
@@ -2,6 +2,5 @@ module.exports = {
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
   mongoURI: process.env.MONGOURI,
-  cookieKey: process.env.COOKIEKEY,
-  googleRedirectURI: "https://young-anchorage-96413.herokuapp.com/"
+  cookieKey: process.env.COOKIEKEY
 };
