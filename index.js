20a7c5348ec0f339cf1a85d690ea9081d87e316e
nahidhasan <nahidhasan@finder-lbs.com>
2018-08-08 13:51:19 +0600

    changed name

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var app = express();
 var PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res, next) => {
-  res.send({ Hello: "Welcome" });
+  res.send({ Name: "Nahid" });
 });
 
 app.listen(PORT);
